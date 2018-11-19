a8196938ce446ba3ec894ebcbf611846186a08f6
DeadlyMelqn <dailytroll92@gmail.com>
2018-11-18 21:25:55 -0700

    Refactor use of class, comment code and refine ... etc and more incremental commits now.

--- calculator-summ.py
+++ calculator-summ.py
@@ -18,53 +18,21 @@ list = allows you to store multiple values
 """
 
 ## WORLD/ROBOT SETTINGS ##
+# create environment automatically, drastically reduces processing time
 think(0)
 World("Alone")
-
-items = ["star", "carrot", "token", "strawberry"]
-for item in items:
+remove_robots()
+reeborg = UsedRobot(1, 10)
+for item in ["star", "carrot", "token", "strawberry"]:
     RUR.give_object_to_robot(item,100)
 
-## VARIABLES ##
+## GLOBAL VARIABLES ##
+loop = 1
 
-## DEFINITIONS ##
-
-# Manipulate turn_left() to make a definiton to turn right, left, or look behind
-def turn(x):
-    if x == "left" or not x:
-        turn_left()
-    elif x == "right" or x == 1:
-        for i in range(0,3):
-            turn_left()
-    elif x == "behind" or x == 2:
-        for i in range(0,2):
-            turn_left()
-    else:
-        raise ReeborgError(" ! You can only move behind, left, right")
-#Create a better move function that allows you to choose the distance Reeborg moves
-#default parameter of 1
-def adv_move(x=1):
-    for i in range(0,x):
-        if not wall_in_front():
-            move()
-        else:
-            print(" ! Cannot move any further. Exiting function")
-            break
-#Allows you to put a specific amount and still specificy amount of carrots
-def adv_put(x=1,y="star"):
-    if x == 0:
-        if y == "star":
-            put("token")
-        elif y == "carrot":
-            put("strawberry")
-        else:
-            raise ReeborgError(" ! What to substitute zero with?")
-    for i in range(0,x):
-        put(y)
-
-## - CALCULATING ##
-class portion1:
-    operators=['add', 'sub', 'mul', 'mod']
+## CLASSES AND DEFINITONS ##
+# CALCULATING #
+class calculation:
+    #operators=['add', 'sub', 'mul', 'mod']
 
     def __init__(self, operator, num1, num2):
         self.operator = operator
@@ -76,7 +44,7 @@ class portion1:
     #        if op in self.operator:
     #            self.operator = operators[i]
 
-    def main(self):
+    def main_p1(self):
         self.total = 0
         self.remainder = 0
         if "add" in self.operator:
@@ -98,114 +66,145 @@ class portion1:
     def returnValue(self):
         return [self.num1, self.num2, self.total, self.remainder]
 
-# - PLOTTING ##
-class portion2:
-    def __init__(self, calc):
-        self.calc = [calc]
-
-    def digits(self, x):
-        count = 0
-        while (x > 0):
-            x = x // 10
-            count = count + 1
-        return count
-
-    def split_digits(self, x):
-        if (x < 10):
-            return [x]
-        else:
-            return self.split_digits(x // 10) + [x % 10]
-
-    def move_lane(self, x):
+# Functions that better enable and improve reeborg's ability to move and etc
+#manipulate turn_left() to make a definiton to turn right, left, or look behind
+def turn(x):
+    if x == "left" or not x:
+        turn_left()
+    elif x == "right" or x == 1:
+        for i in range(0,3):
+            turn_left()
+    elif x == "behind" or x == 2:
         for i in range(0,2):
-            adv_move()
-            turn(j)
+            turn_left()
+    else:
+        raise ReeborgError(" ! You can only move behind, left, right")
+#now define amount of steps and stop running into walls
+def adv_move(x=1):
+    for i in range(0,x):
+        if not wall_in_front():
+            move()
+        else:
+            print(" ! Cannot move any further. Exiting function")
+            break
+#allows you to put a specific amount and appropiately substitute zeros
+def adv_put(x=1,y="star"):
+    if x == '0':
+        if y == "star":
+            put("token")
+        elif y == "carrot":
+            put("strawberry")
+        else:
+            raise ReeborgError(" ! What to substitute zero with?")
+    else:
+        for i in range(0,x):
+            put(y)
+#move down appropiatly and reset to plot rest of equation
+# TODO: remove
+def move_lane(x):
+    for i in range(0,2):
+        adv_move()
+        turn(x)
 
-    def main(self):
-        plot = {}
+# PLOTTING #
 
-        digit_amount = self.digits(max(self.calc))
+# Functions to fulfill plotting requirements by dividing and/or modulus.
+# Could have just used string and then the length of it.
+#count amount of digits
+def digits(x):
+    count = 0
+    while (x > 0):
+        x = x // 10
+        count = count + 1
+    return count
+#split digits into list
+def split_digits(x):
+    if (x < 10):
+        return [x]
+    else:
+        return split_digits(x // 10) + [x % 10]
 
-        for i, v in enumerate(self.calc[:-1]):
-                plot = self.split_digits(v)
+def main(x):
+    calc = x
+    plot = {}
 
-                placeholder = digit_amount - self.digits(v)
-                if (placeholder > 0):
-                    for c in range(0,placeholders):
-                        plot[i].insert(0,"a")
+    digit_amount = digits(max(calc))
+    print("DEBUG: digit_amount is %d" % (digit_amount))
 
-                for k, d in enumerate(plot[i]):
-                    if i:
-                        adv_move()
-                        turn("right")
-                        build_wall()
-                        turn("left")
-                        adv_put(d,"star")
-                    else:
-                        adv_move()
-                        adv_put(d,"star")
+    for i, value in enumerate(calc[:-1]):
+        print("DEBUG: i is %d and value is %d" % (i, value))
+        plot[i] = split_digits(value)
+        print("DEBUG: before placeholders", plot)
 
-                if i != 2:
-                    self.move_lane("right")
-                    adv_move(digit_amount + 1)
-                    turn("behind")
-                else:
-                    if (calc[4] > 0):
-                        for h, s in enumerate(plot[4]):
-                            adv_move()
-                            adv_put(d,"carrot")
+        if i != 3:
+            if (digit_amount > digits(value)):
+                placeholders = digit_amount - digits(value)
+                print("DEBUG: placeholders is %d" % (placeholders))
+                for c in range(0,placeholders):
+                    plot[i].insert(0,'a')
+                print("DEBUG: after placeholders", plot)
+
+            for k, d in enumerate(plot[i]):
+                print("DEBUG: k is %d and d is %d" % (i, value))
+                if i == 1:
                     adv_move()
+                    turn("right")
+                    build_wall()
                     turn("left")
-                    adv_move(2)
-                    turn_right("right")
+                    adv_put(d,"star")
+                else:
+                    adv_move()
+                    adv_put(d,"star")
+
+        if i != 2:
+            move_lane("right")
+            adv_move(digit_amount + 1)
+            turn("behind")
+        else:
+            if i == 3 and (value > 0):
+                for k, d in enumerate(plot[i]):
+                    adv_move()
+                    adv_put(d,"carrot")
+            adv_move()
+            turn("left")
+            adv_move(2)
+            turn("right")
+
+    print("DEBUG: Finished nested loop")
 
 ## CODE ##
 print("##- Reeborg's Calculator - #")
 print("## - - - - - - - - - - - - #")
 
-
-p1 = portion1("add",21,12)
-p1.main()
-calc = p1.returnValue()
-print(calc)
-p2 = portion2(calc)
-p2.main()
-
 """
-count=0
-loop=1
+Create interface to define how much operations you want to calculate and plot
+TODO: - create count variable and instead create a calculation object for each equation
+      - instead of looping and resetting class object, make multiple class objects and
+      just loop plotting instead
+"""
 while loop == 1:
-    p1 = portion1(
+    c = calculation(
         input("Enter operation: "),
         int(input("Enter a number: ")),
         int(input("Enter a second number: "))
         )
-    p1.main()
-    print("Total: ", p1.total)
-    if (p1.remainder > 0):
-        print("Remainder: ", p1.remainder)
+    c.main_p1()
+    print("Total: ", c.total)
+    if (c.remainder > 0):
+        print("Remainder: ", c.remainder)
 
     print("Plotting ...")
-    p2=portion2(
-        p1.num1,
-        p1.num2,
-        p1.total,
-        p1.remainder,
-        )
-    p2.main()
+    main(c.returnValue())
 
-    del p1
-    del p2
+    del c
 
     choice = input("#- Another Equation? Yes/No")
     if 'y' in choice:
-        count = count + 1
         continue
     elif 'n' in choice:
         loop = 0
         break
     else:
         raise ReeborgError(" ! Invalid response")
-"""
 
 raise ReeborgOK("##- Finished")
